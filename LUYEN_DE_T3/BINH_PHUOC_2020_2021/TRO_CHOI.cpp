bbce1a4e32a75a8cc798089435f4784411306c43
adsovetzky <adsovetzky@gmail.com>
2025-11-18 18:06:08 +0700

    I HATE LAZYGIT WHYYYY, U ARE SO HARD TO LEARNNN...

--- LUYEN_DE_T3/BINH_PHUOC_2020_2021/TRO_CHOI.cpp
+++ LUYEN_DE_T3/BINH_PHUOC_2020_2021/TRO_CHOI.cpp
@@ -1,13 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-
 int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-
-  ll m, n;
-  cin >> m >> n;
+  long long n, m;
+  cin >> n >> m;
+  vector<long long> a(n * m), b, kq;
+  for (long long i = 0; i < m * n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  if (n == 1) {
+    for (long long x : a)
+      cout << x << " ";
+  } else {
+    long long d = 0, dem = 1;
+    for (long long i = 0; i < a.size(); i++) {
+      b.push_back(a[i]);
+      d++;
+      if (d == m) {
+        if (dem % 2 == 0) {
+          sort(b.begin(), b.end(), greater<long long>());
+        }
+        for (long long x : b)
+          cout << x << " ";
+        b.clear();
+        cout << endl;
+        dem++;
+        d = 0;
+      }
+    }
+  }
 }
