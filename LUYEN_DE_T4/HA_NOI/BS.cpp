1e21542edb81e8b700393a8572652177baf2005e
adsovetzky <adsovetzky@gmail.com>
2025-11-28 14:16:01 +0700

    homework 7.2

--- LUYEN_DE_T4/HA_NOI/BS.cpp
+++ LUYEN_DE_T4/HA_NOI/BS.cpp
@@ -1,13 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+bool ktra(long long m) {
+  if (m == 0 || m == 1)
+    return false;
+  for (int i = 2; i <= sqrt(m); i++)
+    if (m % i == 0)
+      return false;
+  return true;
+}
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  ll n, x;
+  long long n, x, d = 0, z;
   cin >> n >> x;
+  if (x <= n)
+    d += 2;
+  if (ktra(x) == false)
+    d += 2;
+  else {
+    cout << d;
+    return 0;
+  }
+  z = sqrt(x);
+  if (z == sqrt(x))
+    d++;
+  cout << d;
+  return 0;
 }
